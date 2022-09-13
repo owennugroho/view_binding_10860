06b05c6e661747355d34c9c169ffbc1b1fe12280
owennugroho <owennugroho15@gmail.com>
2022-09-13 09:05:25 +0700

    Initial commit

---
+++ app/src/main/java/com/owen/view_binding_10860/Task.kt
@@ -0,0 +1,3 @@
+package com.owen.view_binding_10860
+
+data class Task (val chapter:String, val title:String, val details:String)
---
+++ app/src/main/java/com/owen/view_binding_10860/TaskList.kt
@@ -0,0 +1,11 @@
+package com.owen.view_binding_10860
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli genshin impact", "mencari primo buat gacha mama yaemiko tercinta / keging wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek nguli kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama ei?", "buat adu mekanik showcase ayaka pyro"),
+        Task("Chapter 5", "upload ke tiktok", "buat pamer ke view showcase ayaka pyro c6 + bennet c6")
+    )
+}
